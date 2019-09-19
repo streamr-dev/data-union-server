15cc1b62e51a905ca4368208cf0a5ca3484d6a86
Juuso Takalainen <juuso.takalainen@streamr.com>
2019-09-19 11:32:04 +0300

    Debugging event handlers not getting triggered
    Perhaps related to https://github.com/ethers-io/ethers.js/issues/107
    
    So far start_server.js still uses Ganache CLI in separate process, but testing indicates there should be no difference.
    
    Also see monoplasma/test/mocha/watcher that also uses Ganache CLI.

---
+++ scripts/watchEventsTest.js
@@ -0,0 +1,95 @@
+// testing how ethers.js does getPastEvents
+//   and how to make it work reliably with Ganache
+
+const {
+    Contract,
+    Wallet,
+    utils,
+    providers: { JsonRpcProvider, Web3Provider }
+} = require("ethers")
+const onProcessExit = require("exit-hook")
+
+const deployTestToken = require("../test/utils/deployTestToken")
+const sleep = require("../src/utils/sleep-promise")
+
+const startGanache = require("monoplasma/src/utils/startGanache")
+const ganacheLib = require("ganache-core")
+
+const ganacheBlockIntervalSeconds = 0
+const fakeTxCount = 20
+
+const ERC20Mintable = require("../build/ERC20Mintable.json")
+
+let ganache = null
+function stopGanache() {
+    if (ganache) {
+        console.log("Shutting down Ethereum simulator...")
+        ganache.shutdown()
+        ganache = null
+    }
+}
+onProcessExit(stopGanache)
+function error(err) {
+    console.error(err.stack)
+    process.exit(1)
+}
+
+async function start() {
+    //const ganacheLog = () => {}
+    const ganacheLog = msg => {
+        if (msg.match("0x")) {
+            console.log(" <Ganache> " + msg)
+        }
+    }
+
+    // Start Ganache CLI (in a separate process)
+    /*
+    ganache = await startGanache(8263, ganacheLog, ganacheLog, ganacheBlockIntervalSeconds)
+    const provider = new JsonRpcProvider(ganache.httpUrl)
+    const keys = ganache.privateKeys
+    */
+
+    // Start Ganache Core (library)
+    const keys = [
+        "0x1234567812345678123456781234567812345678123456781234567812345678",
+        "0x1234567812345678123456781234567812345678123456781234567812345677",
+        "0x1234567812345678123456781234567812345678123456781234567812345676",
+        "0x1234567812345678123456781234567812345678123456781234567812345675",
+    ]
+    const provider = new Web3Provider(ganacheLib.provider({
+        accounts: keys.map(secretKey => ({ secretKey, balance: "0xffffffffffffffffffffffffff" })),
+        logger: { log: ganacheLog },
+        blockTime: ganacheBlockIntervalSeconds,
+    }))
+
+    const wallets = keys.map(key => new Wallet(key, provider))
+    provider.pollingInterval = 500
+    const network = await provider.getNetwork()
+    console.log(`Deploying test token contract (network id = ${network.chainId})...`)
+    const tokenAddress = await deployTestToken(wallets[0])
+    const tokens = wallets.map(wallet => new Contract(tokenAddress, ERC20Mintable.abi, wallet))
+
+    const startTime = Date.now()
+    const filter = tokens[0].filters.Transfer
+    tokens[0].on(filter, (...args) => {
+        const event = args.pop()
+        console.log(`[${Date.now() - startTime}] Got event at block ${event.blockNumber}`)
+    })
+
+    console.log(`Generating ${fakeTxCount} transactions...`)
+    const blockNumbers = []
+    for (let i = 0; i < fakeTxCount; i++) {
+        console.log(`[${Date.now() - startTime}] Sending tx ${i}`)
+        const from = i % wallets.length
+        const to = (i + 1) % wallets.length
+        const tx = await tokens[from].transfer(wallets[to].address, 1000)
+        const tr = await tx.wait(1)
+        console.log(`[${Date.now() - startTime}] Sent tx ${i}, it went into block ${tr.blockNumber}`)
+        blockNumbers.push(tr.blockNumber)
+    }
+
+    await sleep(provider.pollingInterval * 3)
+
+    process.exit(0)
+}
+start().catch(error)
