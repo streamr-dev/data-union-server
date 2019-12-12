89981ceb7fe0f2479620b7d51c9da39f57588e09
Juuso Takalainen <juuso.takalainen@streamr.com>
2019-12-12 11:27:44 +0200

    CPS-65 fix mocks

--- test/unit/watcher.js
+++ test/unit/watcher.js
@@ -65,7 +65,7 @@ describe("MonoplasmaWatcher", () => {
             await provider.send("evm_mine")
         }
 
-        joinPartChannel = new MockStreamrChannel(secretKey, "dummy-stream-for-watcher-test")
+        joinPartChannel = new MockStreamrChannel("dummy-stream-for-watcher-test")
         store = mockStore(startState, initialBlock, log)
 
         log("Deploying test token and Community contract...")
