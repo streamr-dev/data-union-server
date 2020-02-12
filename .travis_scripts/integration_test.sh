1296e12b52280ed8a3c8bae43fb977111169cae9
Henri Pihkala <hpihkala@users.noreply.github.com>
2020-02-12 17:49:29 +0100

    fix travis, start everything except SUT

--- .travis_scripts/integration_test.sh
+++ .travis_scripts/integration_test.sh
@@ -5,8 +5,8 @@ set -e
 sudo /etc/init.d/mysql stop
 git clone https://github.com/streamr-dev/streamr-docker-dev.git
 sudo ifconfig docker0 10.200.10.1/24
-"$TRAVIS_BUILD_DIR/streamr-docker-dev/streamr-docker-dev/bin.sh" start 5
-"$TRAVIS_BUILD_DIR/streamr-docker-dev/streamr-docker-dev/bin.sh" start ganache
+"$TRAVIS_BUILD_DIR/streamr-docker-dev/streamr-docker-dev/bin.sh" start --all
+"$TRAVIS_BUILD_DIR/streamr-docker-dev/streamr-docker-dev/bin.sh" stop community-product
 
 # Wait for EE to come up
 while true; do
