90652ce1836e2f4d2bd439f255419d5d7cb574ed
Isentropy <jwolff@isentropy.com>
2020-05-05 11:35:18 +0200

    wait for smart contract init (#60)

--- .travis_scripts/integration_test.sh
+++ .travis_scripts/integration_test.sh
@@ -8,5 +8,9 @@ sudo ifconfig docker0 10.200.10.1/24
 "$TRAVIS_BUILD_DIR/streamr-docker-dev/streamr-docker-dev/bin.sh" start --except data-union-server --wait
 
 docker ps
+echo "Waiting for smart-contracts-init to complete..."
+docker wait smart-contracts-init
+echo "... smart-contracts-init done"
+
 node ./scripts/create_user.js
 ./node_modules/.bin/mocha --exit test/integration
