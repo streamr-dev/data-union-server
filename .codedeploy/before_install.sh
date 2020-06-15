567bc5cffb32b6a837383e8380e92518210d512d
Mikhael <mikhael.santos@streamr.com>
2020-06-15 12:48:48 +0300

    Add codedeploy files (#73)

---
+++ .codedeploy/before_install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# Because in staging we use the 'dev' tag we will need to update the image vs updating the tag version on the config file
+
+docker pull streamr/data-union-server:dev
