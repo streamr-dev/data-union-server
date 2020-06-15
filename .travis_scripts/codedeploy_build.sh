47c7938f6c36a0dce4d70516dc840e6251755b21
mikhaelsantos <mikhaelsantos@gmail.com>
2020-06-15 16:41:39 +0300

    Rename Appsepc file

--- .travis_scripts/codedeploy_build.sh
+++ .travis_scripts/codedeploy_build.sh
@@ -3,8 +3,8 @@
 set -ev
 
 # Set .appspec in root
-mv .codedeploy/.appspec.yml .appspec.yml
+mv .codedeploy/appspec.yml appspec.yml
 # Copy bash scripts to be deployed in the tar
-tar -czf "du.tar" .appspec.yml .codedeploy
+tar -czf "du.tar" appspec.yml .codedeploy
 mkdir s3_upload
 mv du.tar s3_upload
